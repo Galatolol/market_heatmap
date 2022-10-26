ffc2026c547caea2b9e326ade937efcdc714081e
Galatolol <Galatolol@protonmail.com>
2022-10-26 23:53:10 +0200

    Add output option

--- main.py
+++ main.py
@@ -1,57 +1,53 @@
-import seaborn as sns
+import os
 import matplotlib.pyplot as plt
-from pandas import isna
-
 
 from process_data import calculate_delta_map
 from utils import import_csv_market
 from plots import generate_plot
 
 
-DELTA_TYPES = ['from_left', 'from_below', 'from_left_and_below']
-
-
 def generate_market_heatmaps(
         img_market_path: str = '',
         csv_market_path: str = '',
-        show_market: bool = True,
-        delta_from_left: bool = True,
-        delta_from_below: bool = True,
+        output_directory_path: str = 'output',
+        show_market: bool = False,
+        delta_from_left: bool = False,
+        delta_from_below: bool = False,
         delta_from_left_and_below: bool = True
 ):
-    GAME = '1894'
-    MARKET_PATH = f'data/{GAME}_market.csv'
+    market = import_csv_market(csv_market_path)
 
-    market = import_csv_market(MARKET_PATH)
+    how_many_to_show = sum([show_market, delta_from_left, delta_from_below, delta_from_left_and_below])
+    fig, axs = plt.subplots(how_many_to_show)
 
-    fig, axs = plt.subplots(4)
-
+    i = 0
     if show_market:
-        generate_plot(market, axs[0], 'Market')
+        ax = axs if how_many_to_show == 1 else axs[i]
+        generate_plot(market, ax, 'Market')
+        i += 1
     if delta_from_left:
         deltas = calculate_delta_map(market, from_left=True)
-        generate_plot(deltas, axs[1], 'From left [Δ%]')
+        ax = axs if how_many_to_show == 1 else axs[i]
+        generate_plot(deltas, ax, 'From left [Δ%]')
+        i += 1
     if delta_from_below:
         deltas = calculate_delta_map(market, from_below=True)
-        generate_plot(deltas, axs[2], 'From below [Δ%]')
+        generate_plot(deltas, ax, 'From below [Δ%]')
+        i += 1
     if delta_from_left_and_below:
         deltas = calculate_delta_map(market, from_left=True, from_below=True)
-        generate_plot(deltas, axs[3], 'From left and below combined [Δ%]')
+        ax = axs if how_many_to_show == 1 else axs[i]
+        generate_plot(deltas, ax, 'From left and below combined [Δ%]')
+        i += 1
 
-    plt.show()
-
+    if not os.path.isdir(output_directory_path):
+        os.makedirs(output_directory_path)
+    plt.savefig(f'{output_directory_path}/Heatmap.png')
 
 
 if __name__ == '__main__':
     GAME = '1894'
     CSV_MARKET_PATH = f'data/{GAME}_market.csv'
 
-    generate_market_heatmaps(csv_market_path=CSV_MARKET_PATH)
-
-    # a = DataFrame(deltas_from_left_and_below)
-    # ax = plt.axes()
-    # heatmap = sns.heatmap(a, annot=True, ax=ax, cmap='Oranges')
-    # plt.show()
-    # a = 2
-
+    generate_market_heatmaps(csv_market_path=CSV_MARKET_PATH, show_market=True)
 
